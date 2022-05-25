bc0953d3a64abfbfee5afd4e5e9865758f33dbef
NAGARRO\ankitagupta <ankita.gupat@nagarro.com>
2022-05-25 10:01:01 +0530

    Fetching of data from api

--- monster-rolodex/src/App.tsx
+++ monster-rolodex/src/App.tsx
@@ -1,5 +1,24 @@
+import React, { useEffect, useState } from 'react';
+
 
 function App() {
+  const [users, setUsers] = useState([null]);
+
+  const fetchData = () => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        setUsers(data);
+        console.log(data);
+      })
+  }
+  
+  useEffect(() => {
+    fetchData()
+  }, []);
+
   return (
     <div><h1>Hello World!!</h1></div>
   );
